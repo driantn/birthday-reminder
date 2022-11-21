c1c6e28dd2b8725c60675daaf34e5d54189eba78
Driant Naun <driantn@gmail.com>
2022-11-22 00:02:46 +0100

    init pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
-export default defineConfig({
-	plugins: [solidPlugin()],
-	server: {
-		port: 3000,
-		host: "localhost",
-	},
-	build: {
-		target: "esnext",
-	},
+export default defineConfig(({ mode }) => {
+	return {
+		plugins: [
+			solidPlugin(),
+			VitePWA({
+				registerType: "autoUpdate",
+				devOptions: {
+					enabled: mode === "development",
+				},
+			}),
+		],
+		server: {
+			port: 3000,
+			host: "localhost",
+		},
+		build: {
+			target: "esnext",
+		},
+	};
 });
